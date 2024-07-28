d719feaf73a5a9f969ad188d8ba0fca6e842dcb5
Vishal Prajapati <85871658+vishalprj@users.noreply.github.com>
2024-07-29 02:22:40 +0530

    🚀 sign-up page UI with api integration

---
+++ auth-app/src/app/login/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { useEffect, useState } from "react"
+const Login = () => {
+    const [user, setUser] = useState({
+        email:"",
+        password:""
+    })
+    return(
+        <h1>HELLO WORLD !!!</h1>
+    )
+}
+
+export default Login
